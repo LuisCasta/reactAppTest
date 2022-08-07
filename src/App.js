19e35b7ed3ccdd69efc5f7a9d6f004872c72c919
LuisCasta <luis.alankasta@hotmail.com>
2022-08-07 18:32:24 -0500

    fetch courses

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {Header} from './Header'
 import {Nav} from './Nav'
 import {Video} from './Meeting'
+import {Course} from './Course'
 import './estilos.css'
 
 
@@ -8,20 +9,32 @@ import './estilos.css'
 
 function App() {
 
-  const getVentos =async () => {
-
-    const result = await fetch("https://ventos-api-server.onrender.com")
-  
-   const ventos = await result.json();
-
-   console.log(ventos);
+  const getVentos = async () => {
+	
+    const url = 'https://ventos-api-server.onrender.com/api/course/';
+    
+    const result = await fetch(url);
+    const ventos = await result.json();
+    
+    ventos.map(course =>{
+      console.log('curso: '+course.curso)
+    })
+    return ventos;
+    
   }
   
-  getVentos()
+  var cursos = getVentos();
 
   return (
+    
     <div>
        <Nav />
+       {
+        
+        cursos.map( curso => {
+          <Course name={curso.curso} description={curso.descripcion}/>
+        })
+       }
        <Header />
        <Video/>
     </div>
---
+++ src/Course.jsx
@@ -0,0 +1,9 @@
+export const Course = ({name, description}) => {
+    
+  return (
+    <>
+        <h1>{name}</h1><br />
+        <p>{description}</p>
+    </>
+  )
+}
